bdad47290941821bd6a48719e53615c3c7e2c8c1
Victória Nascimento <davick006@gmail.com>
2025-11-17 22:52:38 -0300

    inicia novas telas (configuracoes, mensagens e documentos)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import HomePage from "./pages/hospedagens/home-page/home-page";
 import Agendamento from "./pages/hospedagens/agendamento/agendamento";
 import ServicoScreen from "./pages/hospedagens/servico/servico";
 import FuncionarioScreen from "./pages/hospedagens/funcionario/funcionario";
+import Documentos from "./pages/hospedagens/documentos/documentos";
+import Mensagens from "./pages/hospedagens/mensagens/mensagens";
+import Configuracoes from "./pages/hospedagens/configuracoes/configuracoes"; 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
@@ -20,6 +23,9 @@ export default function App() {
           <Route path="/agendamento" element={<Agendamento />} />
           <Route path="/servico" element={<ServicoScreen />} />
           <Route path="/funcionario" element={<FuncionarioScreen />} />
+          <Route path="/documentos" element={<Documentos />} />
+          <Route path="/mensagens" element={<Mensagens />} />
+          <Route path="/configuracoes" element={<Configuracoes />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/hospedagens/configuracoes/configuracoes.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./configuracoes.css";
+import { Link } from 'react-router-dom';
+import { Home, Calendar, Wrench, Users, MessageSquare, FileText, Settings, Boxes } from "lucide-react";
+
+const Configuracoes = () => {
+  return (
+    <div className="container">
+      <aside className="sidebar">
+        <h2 className="logo">PetFamily</h2>
+
+        <nav className="menu">
+          <Link className="menu-item" to="/home"><Home size={16}/> Início</Link>
+          <Link className="menu-item" to="/agendamento"><Calendar size={16}/> Agendamentos</Link>
+          <Link className="menu-item" to="/servico"><Wrench size={16}/> Serviços</Link>
+          <Link className="menu-item" to="/funcionario"><Users size={16}/> Funcionários</Link>
+          <Link className="menu-item" to="/mensagens"><MessageSquare size={16}/> Mensagens</Link>
+          <Link className="menu-item" to="/interacoes"><Boxes size={16}/> Interações</Link>
+          <Link className="menu-item" to="/documentos"><FileText size={16}/> Documentos</Link>
+          <Link className="menu-item active" to="/configuracoes"><Settings size={16}/> Configurações</Link>
+        </nav>
+
+        <button className="logout">⟵ Sair</button>
+      </aside>
+
+      <main className="content">
+        <h1>Configurações</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Configuracoes;
